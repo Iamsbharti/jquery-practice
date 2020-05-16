ace3f61b511331daa561d94c93bb5d3b42cf9118
Saurabh <saurabhbharti9@gmail.com>
2020-05-17 03:46:28 +0530

    called local server using AJAX

--- ajax_util.js
+++ ajax_util.js
@@ -1,5 +1,10 @@
 $(document).ready(() => {
-  showData();
+  $("#btnShow").click(() => {
+    showData();
+  });
+  $("#clearData").click(() => {
+    clearData();
+  });
 });
 let showData = () => {
   $.ajax({
@@ -11,12 +16,17 @@ let showData = () => {
       let astro = data.people;
 
       for (ast of astro) {
-        let row = `<div class="row>
-                    <div class="col">${ast.name}</div>
-                    <div class="col">${ast.craft}</div>
-                   </div>`;
+        let row = `<div class="row">
+                      <div class="col">${ast.name}</div>
+                      <div class="col">${ast.craft}</div>
+                   </div>
+                   `;
         $("#showData").append(row);
       }
     },
   });
+};
+let clearData = () => {
+  console.log("clear");
+  $("#data").children().attr("style", "display: none");
 };
